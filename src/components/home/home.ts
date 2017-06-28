4de98463cb6aff096fdbe7841cde7a00aaf085dd
Yunseop Kim <yunsub2@gmail.com>
2017-06-28 19:12:06 +0900

    1. 퇴근 전 커밋

--- src/components/home/home.ts
+++ src/components/home/home.ts
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Component from 'vue-class-component';
+import {firebaseDB} from '../../firebase/config';
 
 @Component({
     template: require('./home.html')
@@ -9,5 +10,12 @@ export class HomeComponent extends Vue {
     package: string = 'vue-typescript-todo';
     repo: string = 'https://github.com/yunseop-kim/vue-typescript-todo';
     mode: string = process.env.ENV;
+    firebaseVal: string;
 
+    
+    public getValue() {
+        firebaseDB.ref('/Test').once('value').then((snapshot) => {
+            this.firebaseVal = snapshot;
+        });
+    }
 }
